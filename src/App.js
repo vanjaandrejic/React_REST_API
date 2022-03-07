59789731fe53b86c4524f8baeff3f3879a4cdb80
Vanja Andrejic <njavagg@gmail.com>
2022-03-07 23:20:29 +0100

    Adding components and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import Header from './components/Header'
+import Form from './components/Form'
+import Posts from './components/Posts'
+import Post from './components/Post'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+
+    <Routes>
+      
+    <Route exact path="/" element={<>
+
+            <Header/>
+            <div>
+              <Form/>
+            <Posts/>
+            </div>
+            </>}/>
+
+        <Route path='/post' element={<Post/>} />
+
+        </Routes>
+        
+    </Router>
   )
 }
 
